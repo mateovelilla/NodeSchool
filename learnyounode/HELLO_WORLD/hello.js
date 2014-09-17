cc4ab3ff389bace3e553d78ce84c7ad239528b1d
mateovelilla <teovelilla@gmail.com>
2014-09-17 01:08:18 -0500

    Inicio del camino node

---
+++ learnyounode/HELLO_WORLD/hello.js
@@ -0,0 +1,25 @@
+/*
+LEARN YOU THE NODE.JS FOR MUCH WIN!
+─────────────────────────────────────
+En este primer ejercicio en pocas palabras te dice que escribas con javascript
+un "Hola mundo", pero espeficamente tiene que estar escrita "HELLO WORLD".
+
+Tenemos que tener presente que NODE( de cariño ) es un entorno de desarrollo basado en javascript del lado
+de SERVIDOR y como todo servidor se maneja desde la terminal, a si que abramos nuestras terminales :D 
+y despues de haber instalado node.js y el modulo de nodeschool learnyounode creamos un archivo con extension JS
+imprimiendo nuestro primer "HOLA MUNDO" con javascript. Existen 3 formas de verificar si el ejercicio funciona realmente:
+
+- NombreDelmodulo verify NombreArchivo = esta sentencia verifica el ejercicio seleccionado anteriormente seleccionado
+en el menu del modulo en este caso el ejercicio "HELLO WORLD", y el modulo "learnyounode".Ej: learnyounode verify hello.js
+NOTA: para ejecutar el comando hay que estar ubicado desde la terminal donde se encuentra ubicado el archivo.
+
+- node NombreArchivo = este comando compila y ejecuta todo el codigo javascript que se encuentra en el archivo seleccionado en
+el comando. Ej: hello.js
+
+-NombreDelModulo run NombreArchivo = este comando ejecuta el archivo javascript y si existen excepciones que corresponden
+a las instrucciones del ejercicio no los resaltara, solo cuando las excepciones sean del lenguaje.
+
+
+*/
+
+console.log("HELLO WORLD");
