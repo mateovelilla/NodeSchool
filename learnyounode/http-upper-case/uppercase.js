f5a4a2557dd54c0fbeaf46a416ccfef52419d4f8
MateoVelilla <teovelilla@gmail.com>
2017-09-05 09:59:34 -0500

    Anexo de httpUpperCase

---
+++ learnyounode/http-upper-case/uppercase.js
@@ -0,0 +1,16 @@
+let http = require('http'),
+	port = process.argv[2],
+	map = require('through2-map')
+
+let server = http.createServer(function (req , res){
+	if(req.method == 'POST'){
+		req.pipe(map(function (chunk){
+			return chunk.toString()
+				.toUpperCase()
+		}))
+		.pipe(res);
+	}else{
+		req.end();
+	}
+}).listen(port);
+
