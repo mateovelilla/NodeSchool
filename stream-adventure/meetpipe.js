df77206fce3f23f46f2567ff41bfd451dd546eb3
MateoVelilla <teovelilla@gmail.com>
2018-03-12 16:17:49 -0500

    stream-adventure

---
+++ stream-adventure/meetpipe.js
@@ -0,0 +1,4 @@
+let file = process.argv[2];
+const fs = require('fs');
+fs.createReadStream(file)
+	.pipe(process.stdout);
---
+++ stream-adventure/transform/bufferTransform.js
@@ -0,0 +1,9 @@
+'use strict';
+let through = require('through2')
+let stream = through(function(buffer, enconding, next){
+	let text = buffer.toString().toUpperCase()
+	this.push(text)
+	next()
+})
+process.stdin.pipe(stream).pipe(process.stdout)
+
