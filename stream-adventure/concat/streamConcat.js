07040e39d10ff8a5c71ba4ce4b601183868c23f1
MateoVelilla <teovelilla@gmail.com>
2018-03-13 10:53:57 -0500

    Buffer concat

---
+++ stream-adventure/concat/streamConcat.js
@@ -0,0 +1,7 @@
+'use strict';
+const concat = require('concat-stream')
+let concatStream = concat(function(buffer){
+	let bufferToString = buffer.toString().split("").reverse().join("")
+	process.stdout.write(bufferToString)
+})
+process.stdin.pipe(concatStream)
